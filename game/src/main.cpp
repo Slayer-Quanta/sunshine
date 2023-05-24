b77ee43cf4acd818427fc199e42593e9d9801664
Slayer-Quanta <91901675+Slayer-Quanta@users.noreply.github.com>
2023-05-24 02:57:10 -0400

    Added circles and collision

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,4 +1,6 @@
 #include "rlImGui.h"
+#include "raylib.h"
+
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
@@ -8,24 +10,40 @@ int main(void)
 	InitAudioDevice();
 	SetTargetFPS(60);
 
-	Texture Enterprise = LoadTexture("../game/assets/textures/enterprise.png");
+	Texture2D enterprise = LoadTexture("../game/assets/textures/enterprise.png");
 	Music music = LoadMusicStream("../game/assets/audio/DANGERZONE.MP3");
-	Sound Yay = LoadSound("../game/assets/audio/yay.ogg");
-	PlaySound(Yay);
+	Sound yay = LoadSound("../game/assets/audio/yay.ogg");
+	PlaySound(yay);
 	PlayMusicStream(music);
+
+	Vector2 circlePosition = { 400, 300 };
+	float circleRadius = 50;
+	Color circleColor = RED;
 
 	while (!WindowShouldClose())
 	{
 		UpdateMusicStream(music);
 
 		BeginDrawing();
-			ClearBackground(RAYWHITE);
-			DrawText("Hello World",16,9, 20, RED);
-			DrawTexture(Enterprise, 0, 0, WHITE);
-			EndDrawing();
+
+		ClearBackground(RAYWHITE);
+
+		DrawText("Hello World", 16, 9, 20, RED);
+		DrawTexture(enterprise, 0, 0, WHITE);
+
+		DrawCircle(circlePosition.x, circlePosition.y, circleRadius, circleColor);
+		Vector2 mousePosition = GetMousePosition();
+		bool collision = CheckCollisionCircles(circlePosition, circleRadius, mousePosition, circleRadius);
+		if (collision)
+			circleColor = BLUE;
+		else
+			circleColor = RED;
+		DrawCircle(mousePosition.x, mousePosition.y, circleRadius, circleColor);
+		EndDrawing();
 	}
-	UnloadTexture(Enterprise);
-	UnloadSound(Yay);
+
+	UnloadTexture(enterprise);
+	UnloadSound(yay);
 	UnloadMusicStream(music);
 	CloseAudioDevice();
 	CloseWindow();
